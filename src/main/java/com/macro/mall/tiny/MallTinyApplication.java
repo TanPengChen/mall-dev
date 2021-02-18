2bcc9024a95cb999ab2c141584ef03dd14c40912
tanpeng <tanpengchen@126.com>
2021-02-18 14:04:52 +0800

    去掉多余依赖

--- src/main/java/com/macro/mall/tiny/MallTinyApplication.java
+++ src/main/java/com/macro/mall/tiny/MallTinyApplication.java
@@ -3,7 +3,6 @@ package com.macro.mall.tiny;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.macro.mall.tiny.service")
